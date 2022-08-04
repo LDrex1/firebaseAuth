062d4956815ea580dfaff1fa2691ef56e7f4bb69
lDrex1 <damzealwini7@gmail.com>
2022-08-04 14:01:59 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const googleBtn = document.querySelector("#google-register");
 
 const googleAuth = async () => {
-  googleBtn ? googleBtn.addEventListener("click", googleAuth) : null;
   const googleAuthProvider = new firebase.auth.googleAuthProvider();
   await firebase
     .auth()
     .signINWithPopup(googleAuthProvider)
     .then(() => window.open());
-  googleBtn.removeEventListener("click", googleAuth);
+  //   googleBtn.removeEventListener("click", googleAuth);
 };
+googleBtn ? googleBtn.addEventListener("click", googleAuth) : null;
